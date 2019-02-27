808066cee88446e1cd68c3db175df11ed755bc08
Jaskirat Billing <Billingjas22@gmail.com>
2019-02-26 20:32:09 -0800

    first commit

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const user = express.Router();
+
+user.get('/', function(req, res, next) {
+  res.render('user');
+});
+
+user.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+user.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+module.exports = user;
